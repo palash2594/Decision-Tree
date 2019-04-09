f205a2c33e2605cc7e59d514ce3bdadbdea4e4d0
palash jain <palashjain2508@gmail.com>
2019-04-08 21:21:33 -0400

    Bayesian network improvements

--- src/bayesian network/bayes.py
+++ src/bayesian network/bayes.py
@@ -1,19 +1,36 @@
 import numpy as np
 from scipy.stats import norm
-
+import math
+import random
 
 N = 7
 Y = 3
 
-def load_data(filename):
-    data = np.array([[float(x) for x in line.strip().split('\t')] for line in open(filename).readlines()])
-    # print('Loaded %d observations.'%len(data))
+def read_data(file_name):
+    data = []
+
+    file_data = open("seeds_dataset.txt", "r")
+
+    for line in file_data:
+        line = line.strip().split("\t")
+        temp = []
+        for item in line[0:len(line)]:
+            temp.append(float(item))
+        data.append(temp)
+    file_data.close()
+
     return data
 
+# def load_data(filename):
+#     data = np.array([[float(x) for x in line.strip().split('\t')] for line in open(filename).readlines()])
+#     # print('Loaded %d observations.'%len(data))
+#     return data
+
 def condprob(x, n, y, params):
-    temp = norm.pdf(x, params[n][y]['mean'], params[n][y]['var'])\
-        # print(x, params[n][y]['mean'], params[n][y]['var'])
-    return temp
+    mu = params[n][y]['mean']
+    sigma = params[n][y]['var']
+    pdf = (1 / (sigma * math.sqrt(2 * 3.14))) * math.exp(-1 * math.pow(x - mu, 2) / (2 * math.pow(sigma, 2)))
+    return pdf
 
 def learn(data):
     params = {}
@@ -43,21 +60,22 @@ def classify(obs, params):
 
 
 def demo():
-    train_data = load_data('seeds_dataset.txt')
-    params = learn(train_data)
+    data = read_data('seeds_dataset.txt')
+    train_data = random.sample(data, k = int(0.3 * 210))
+    test_data = random.sample(data, k = int(0.7 * 210))
 
-    test_data = load_data('seeds_dataset.txt')
+    params = learn(train_data)
 
     correct = 0
     np.seterr(divide='ignore', invalid='ignore')
     for obs in test_data:
-        # print(obs, end='-- ')
+        print(obs, end='-- ')
         result = classify(obs, params)
         result = np.array(result) / np.sum(result)
         if np.argmax(result) == obs[-1]:
             correct += 1
-        # print(result, end=' -- ')
-        # print(np.argmax(result))
+        print(result, end=' -- ')
+        print(np.argmax(result))
     print('accuracy:', correct / len(test_data))
 
 if __name__ == '__main__':
